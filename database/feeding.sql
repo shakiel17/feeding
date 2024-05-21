f0514f7803a0a35cca0c6469e2bb5a3533782001
shakiel17 <hisoka.aboy@gmail.com>
2024-05-21 15:36:22 +0800

    Create feeding.sql

---
+++ database/feeding.sql
@@ -0,0 +1,114 @@
+/*
+SQLyog Community v13.1.6 (64 bit)
+MySQL - 10.1.38-MariaDB : Database - feeding
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`feeding` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `feeding`;
+
+/*Table structure for table `admin` */
+
+DROP TABLE IF EXISTS `admin`;
+
+CREATE TABLE `admin` (
+  `id` int(45) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `fullname` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `admin` */
+
+/*Table structure for table `dispensing` */
+
+DROP TABLE IF EXISTS `dispensing`;
+
+CREATE TABLE `dispensing` (
+  `id` int(45) NOT NULL AUTO_INCREMENT,
+  `fish_id` int(11) DEFAULT NULL,
+  `stock_id` int(11) DEFAULT NULL,
+  `quantity` double DEFAULT NULL,
+  `datearray` date DEFAULT NULL,
+  `timearray` time DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `dispensing` */
+
+/*Table structure for table `fish` */
+
+DROP TABLE IF EXISTS `fish`;
+
+CREATE TABLE `fish` (
+  `id` int(45) NOT NULL AUTO_INCREMENT,
+  `description` text,
+  `category` varchar(100) DEFAULT NULL,
+  `feed_usage` double DEFAULT NULL,
+  `datearray` date DEFAULT NULL,
+  `timearray` time DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `fish` */
+
+/*Table structure for table `notification` */
+
+DROP TABLE IF EXISTS `notification`;
+
+CREATE TABLE `notification` (
+  `id` int(45) NOT NULL AUTO_INCREMENT,
+  `stock_id` int(11) DEFAULT NULL,
+  `datearray` date DEFAULT NULL,
+  `timearray` time DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `notification` */
+
+/*Table structure for table `purchaseorder` */
+
+DROP TABLE IF EXISTS `purchaseorder`;
+
+CREATE TABLE `purchaseorder` (
+  `id` int(45) NOT NULL AUTO_INCREMENT,
+  `pono` varchar(100) DEFAULT NULL,
+  `stock_id` int(11) DEFAULT NULL,
+  `quantity` double DEFAULT NULL,
+  `datearray` date DEFAULT NULL,
+  `timearray` time DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `purchaseorder` */
+
+/*Table structure for table `stocks` */
+
+DROP TABLE IF EXISTS `stocks`;
+
+CREATE TABLE `stocks` (
+  `id` int(45) NOT NULL AUTO_INCREMENT,
+  `description` text,
+  `quantity` double DEFAULT NULL,
+  `stockalert` double DEFAULT NULL,
+  `datearray` date DEFAULT NULL,
+  `timearray` time DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `stocks` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
